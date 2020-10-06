1e37655ab65c95db03d086f86a361a9e39b4d51e
EventlessKarma <max-porter@outlook.com>
2020-10-06 22:41:04 +0100

    broke it and made it better

--- field_lines.py
+++ field_lines.py
@@ -4,6 +4,107 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
+class MagneticField:
+    def __init__(self, field: list, *params):
+        # argument field expects list of field components in cartesians
+        # argument params for arbitrary number of parameters to be used in field
+        # length of params must represent number of params in field
+
+        # store field and input parameters
+        self.params = params
+
+        self.bx = field[0]
+        self.by = field[1]
+        self.bz = field[2]
+
+    def magnitude(self, pos):
+        # calculate the magnitude of the field at a particular pos(x, y, z)
+        return np.sqrt(
+            np.power(self.bx(pos, *self.params), 2) +
+            np.power(self.by(pos, *self.params), 2) +
+            np.power(self.bz(pos, *self.params), 2)
+        )
+
+
+# to arrange field equations in form used for ode solver
+def to_solve_field_lines(s, pos: list, field: MagneticField):
+    mag = field.magnitude(pos)
+    return [field.bx(pos, *field.params)/mag,
+            field.by(pos, *field.params)/mag,
+            field.bz(pos, *field.params)/mag]
+
+
+# generate list of field line origins
+def get_line_begin(x: list, y: list, z: list):
+    start = []
+    for x in np.linspace(0.1, 2, 1):
+        for y in np.linspace(0.1, 2, 1):
+            for z in np.linspace(0.1, 2, 1):
+                start.append([x, y, z])
+    return start
+
+
+# generate field lines
+def generate_lines(path, initial_pos: list, mag_field: MagneticField):
+    # path arg as list of path length values
+
+    # find whether multiple lines are expected
+    initial_pos = np.array(initial_pos)
+    if initial_pos.ndim == 1:  # one field line expected
+        sol = solve_ivp(lambda s, pos: to_solve_field_lines(s, pos, mag_field),
+                        [path[0], path[-1]],
+                        initial_pos,
+                        t_eval=path)
+        return sol
+
+    else:
+        sols = []
+        for pos in initial_pos:  # multiple field lines expected
+            sols.append(solve_ivp(lambda s, pos: to_solve_field_lines(s, pos, mag_field),
+                                  [path[0], path[-1]],
+                                  pos,
+                                  t_eval=path))
+        return sols
+
+
+# UNIFORM FIELD EXAMPLE
+steps = np.linspace(0, 5, 1000)  # generate path length, smaller step length increases accuracy
+# input field component functions
+uniform_field = MagneticField([
+    lambda pos: 0,
+    lambda pos: 3,  # static field in y
+    lambda pos: 0])
+ini = [[1, 1, 0], [2, 1, 0], [3, 1, 0]]  # line origins
+lines = generate_lines(steps, ini, uniform_field)  # list of line solutions
+
+# plot all lines
+plt.figure()
+for line in lines:
+    plt.plot(line.y[0], line.y[1])
+
+
+# ABC FIELD EXAMPLE
+steps = np.linspace(0, 3000, 1000000)
+abc_field = MagneticField([
+    lambda pos, a, b, c, lamb: a*np.sin(lamb*pos[2]) + c*np.cos(lamb*pos[1]),
+    lambda pos, a, b, c, lamb: b*np.sin(lamb*pos[0]) + a*np.cos(lamb*pos[2]),
+    lambda pos, a, b, c, lamb: c*np.sin(lamb*pos[1]) + b*np.cos(lamb*pos[0])],
+    1, np.sqrt(2/3), np.sqrt(1/3), 1
+)
+ini = [0.213, 0.342, 0.001]  # one field line
+lines = generate_lines(steps, ini, abc_field)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.plot(lines.y[0], lines.y[1], lines.y[2])
+
+plt.grid()
+plt.show()
+
+
+
+
+"""
 
 class MagneticField:
     def __init__(self, parameters):
@@ -56,3 +157,4 @@ plt.grid()
 #ax.set_xlim(0, 2)
 
 plt.show()
+"""
