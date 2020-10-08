1be41aa769a271ffe510da4df640c756d741a779
EventlessKarma <max-porter@outlook.com>
2020-10-08 15:24:19 +0100

    not much

--- wirefield.py
+++ wirefield.py
@@ -18,9 +18,6 @@ def cal_err(be, sol):
     for i in range(len(p)):
         p[i] = p[i] / (2*math.pi*r)
 
-    print('start = ({}, {})'.format(be[0], be[1]))
-    print('end   = ({}, {})'.format(fin[0], fin[1]))
-    print('x = {:.3f}%   y = {:.3f}%'.format(p[0], p[1]))
     return p, fin
 
 
@@ -30,11 +27,12 @@ START = 0
 STOP = 20
 
 steps = np.arange(start=START, stop=STOP, step=STEP_LENGTH)
-# steps = np.linspace(0, 14.2, 100000)
+
 wire_field = field_lines.MagneticField([
     lambda pos: (1/(pos[0]**2+pos[1]**2))*(-pos[1]),
     lambda pos: (1/(pos[0]**2+pos[1]**2))*(pos[0]),
     lambda pos: 0])
+
 ini = [[1, 2, 1], [1, 2, 2], [1, 2, 3], [1, 1, 1], [1, 1, 2], [1, 1, 3]]
 
 fig = plt.figure()
@@ -43,14 +41,27 @@ lines_RK = field_lines.generate_lines(steps, ini, wire_field)
 lines_DOP = field_lines.generate_lines(steps, ini, wire_field, method='DOP853')
 
 ax = fig.add_subplot(111, projection='3d')
-for line, begin in zip(lines_RK, ini):
-    plt.plot(line.y[0], line.y[1], line.y[2], color='blue')
+for line_RK, line_DOP, begin in zip(lines_RK, lines_DOP, ini):
 
-    err, f = cal_err(begin, line)
-    ax.scatter(f[0], f[1], f[2], color='blue')
+    print('start = ({}, {})'.format(begin[0], begin[1]))
+
+    plt.plot(line_RK.y[0], line_RK.y[1], line_RK.y[2], color='blue')
+    plt.plot(line_DOP.y[0], line_DOP.y[1], line_DOP.y[2], color='red')
+
+    err_RK, f_RK = cal_err(begin, line_RK)
+    err_DOP, f_DOP = cal_err(begin, line_DOP)
+
+    ax.scatter(f_RK[0], f_RK[1], f_RK[2], color='blue')
+    ax.scatter(f_DOP[0], f_DOP[1], f_DOP[2], color='red')
+    print('RK end   = ({}, {})'.format(f_RK[0], f_RK[1]))
+    print('x = {:.3f}%   y = {:.3f}%'.format(err_RK[0], err_RK[1]))
+
+    print('DOP end   = ({}, {})'.format(f_DOP[0], f_DOP[1]))
+    print('x = {:.3f}%   y = {:.3f}%'.format(err_DOP[0], err_DOP[1]))
+
     ax.scatter(begin[0], begin[1], begin[2], color='black')
 
-
+"""
 print("____ DOP853 _____")
 # ax = fig.add_subplot(111, projection='3d')
 for line, begin in zip(lines_DOP, ini):
@@ -59,6 +70,6 @@ for line, begin in zip(lines_DOP, ini):
     err, f = cal_err(begin, line)
     ax.scatter(f[0], f[1], f[2], 'red')
     ax.scatter(begin[0], begin[1], begin[2], color='black')
-
+"""
 
 plt.show()
