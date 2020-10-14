f10c1bdb2f38d96528b6628f0c986803b582f3e4
EventlessKarma <max-porter@outlook.com>
2020-10-15 00:12:28 +0100

    abc in c++ added

---
+++ testing_abc_with_c.py
@@ -0,0 +1,18 @@
+import numpy as np
+import expon  # THIS IS THE ABC FIELD MODULE
+import matplotlib.pyplot as plt
+
+# PARAMS
+s_start = 0
+s_end = 1000
+step_length = 0.01  
+x_ini = [1, 2, 1]  # initial x, y, z positions
+params = [1, 2, 2, 1]  # a, b, c, lambda
+
+abc = expon.abc_field(s_start, s_end, step_length, x_ini, params)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.plot(abc[1], abc[2], abc[3])
+plt.show()
+
