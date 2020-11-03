1de010bb40e919a12c852d8312b8a9bd203c299a
EventlessKarma <max-porter@outlook.com>
2020-11-03 12:17:15 +0000

    plot one upgrade

--- abc_field.py
+++ abc_field.py
@@ -198,6 +198,8 @@ def plot_one(ss, se, size, ini, param):
 
 
 def plot_one_periodic(ss, se, size, ini, param):
+    lw = 0.1
+
     print('expected time = {}'.format(estimate_duration(se/size)))
     t0 = time.time()
     line = solvefields.abc_field(ss, se, size, ini, param)
@@ -206,30 +208,52 @@ def plot_one_periodic(ss, se, size, ini, param):
     lz = solvefields.periodic_projection(line.z)
 
     fig = plt.figure()
-    traj = fig.add_subplot(projection='3d')
 
+    traj = fig.add_subplot(121, projection='3d')
     traj.plot(ini[0], ini[1], ini[2], color='black', marker='x')
-    traj.plot(lx, ly, lz, color='purple', linewidth=0.1)
+    traj.plot(line.x, line.y, line.z, color='green', linewidth=lw)
     traj.set_xlabel('x')
     traj.set_ylabel('y')
     traj.set_zlabel('z')
 
+    traj_per = fig.add_subplot(122, projection='3d')
+    traj_per.plot(ini[0], ini[1], ini[2], color='black', marker='x')
+    traj_per.plot(lx, ly, lz, color='blue', linewidth=lw)
+    traj_per.set_xlabel('$x/2\pi$')
+    traj_per.set_ylabel('$y/2\pi$')
+    traj_per.set_zlabel('$z/2\pi$')
+
     fog = plt.figure()
 
-    x = fog.add_subplot(221)
-    x.plot(line.s, lx, color='green')
+    x = fog.add_subplot(231)
+    x.scatter(line.s, lx, color='green', s=lw)
     x.set_xlabel('s')
     x.set_ylabel('x')
 
-    y = fog.add_subplot(222)
-    y.plot(line.s, ly, color='green')
+    y = fog.add_subplot(232)
+    y.scatter(line.s, ly, color='green', s=lw)
     y.set_xlabel('s')
     y.set_ylabel('y')
 
-    z = fog.add_subplot(223)
-    z.plot(line.s, lz, color='green')
+    z = fog.add_subplot(233)
+    z.scatter(line.s, lz, color='green', s=lw)
     z.set_xlabel('s')
     z.set_ylabel('z')
+
+    xy = fog.add_subplot(234)
+    xy.scatter(lx, ly, color='blue', s=lw)
+    xy.set_xlabel('$x/2\pi$')
+    xy.set_ylabel('$y/2\pi$')
+
+    xz = fog.add_subplot(235)
+    xz.scatter(lx, lz, color='blue', s=lw)
+    xz.set_xlabel('$x/2\pi$')
+    xz.set_ylabel('$z/2\pi$')
+
+    yz = fog.add_subplot(236)
+    yz.scatter(ly, lz, color='blue', s=lw)
+    yz.set_xlabel('$y/2\pi$')
+    yz.set_ylabel('$z/2\pi$')
 
     t1 = time.time()
     print('time taken = {}'.format(t1-t0))
@@ -658,7 +682,7 @@ if __name__ == '__main__':
     # dobre_zero_c()
     # projection()
     #plot_one(0, 10000, 0.1, [0.3*2*np.pi, 0.2*2*np.pi, 0.2*2*np.pi], [1, 2, 1, 1])
-    plot_one_periodic(0, 10000, 0.1, [0.3*2*np.pi, 0.2*2*np.pi, 0.2*2*np.pi], [1, 2, 3, 4])
+    plot_one_periodic(0, 1000, 0.1, [0.3*2*np.pi, 0.2*2*np.pi, 0.2*2*np.pi], [1, 2, 3, 4])
 
     # multi_projection(s=0, e=3000, st=0.1, n=5, p=[1, 2, 1, 1])
     #test_projection()
